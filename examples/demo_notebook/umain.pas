d02a2dcf47ed86481342309ee431f9c0671f679a
Ovidio Peña Rodríguez <ovidiopr@users.noreply.github.com>
2026-06-09 09:59:38 +0200

    Updated demos

--- examples/demo_notebook/umain.pas
+++ examples/demo_notebook/umain.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, ValEdit,
-  StdCtrls, ExtDlgs, Interfaces, ExtTabCtrl;
+  StdCtrls, Buttons, ExtDlgs, ExtTabCtrl;
 
 type
 
@@ -14,9 +14,13 @@ type
 
   TForm1 = class(TForm)
     Button1: TButton;
+    cbShowTabStripes: TCheckBox;
+    cbShowAddButton: TCheckBox;
+    cbShowCloseButtons: TCheckBox;
     CmbTabStyle: TComboBox;
     ExtTabCtrl1: TExtTabCtrl;
     Image1: TImage;
+    ImageList1: TImageList;
     LblTabStyle: TLabel;
     Notebook1: TNotebook;
     OpenPictureDialog1: TOpenPictureDialog;
@@ -27,14 +31,22 @@ type
     Panel1: TPanel;
     ValueListEditor1: TValueListEditor;
     procedure Button1Click(Sender: TObject);
+    procedure cbShowAddButtonChange(Sender: TObject);
+    procedure cbShowCloseButtonsChange(Sender: TObject);
+    procedure cbShowTabStripesChange(Sender: TObject);
     procedure CmbTabStyleChange(Sender: TObject);
     procedure ExtTabCtrl1TabChanged(Sender: TObject; NewIndex: Integer);
-    procedure ExtTabCtrl1TabCreating(Sender: TObject; var ACaption: String; var Data: TObject; var Allow: Boolean);
-    procedure ExtTabCtrl1TabDeleting(Sender: TObject; Index: Integer; var Allow: Boolean);
-    procedure ExtTabCtrl1TabReordered(Sender: TObject; OldIndex, NewIndex: Integer);
+    procedure ExtTabCtrl1TabCreated(Sender: TObject);
+    procedure ExtTabCtrl1TabCreating(Sender: TObject; var ACaption: String;
+      var Data: TObject; var Allow: Boolean);
+    procedure ExtTabCtrl1TabDeleting(Sender: TObject; Index: Integer;
+      var Allow: Boolean);
+    procedure ExtTabCtrl1TabReordered(Sender: TObject; OldIndex,
+      NewIndex: Integer);
     procedure FormCreate(Sender: TObject);
   private
     procedure LoadImage(const AFileName: String);
+
   public
 
   end;
@@ -50,8 +62,9 @@ implementation
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
-  ExtTabCtrl1.AutoSize := True;
   CmbTabStyle.ItemIndex := ord(ExtTabCtrl1.TabStyle);
+  ExtTabCtrl1.AutoSize := True;
+  ExtTabCtrl1.TabIndex := 0;
 end;
 
 procedure TForm1.ExtTabCtrl1TabChanged(Sender: TObject; NewIndex: Integer);
@@ -60,13 +73,24 @@ begin
     Notebook1.PageIndex := NewIndex;
 end;
 
-procedure TForm1.ExtTabCtrl1TabCreating(Sender: TObject; var ACaption: String; var Data: TObject; var Allow: Boolean);
+procedure TForm1.ExtTabCtrl1TabCreated(Sender: TObject);
 begin
-  Notebook1.Pages.Add(ACaption);
+  if cbShowTabStripes.Checked then
+    ExtTabCtrl1.Tabs[ExtTabCtrl1.Tabs.Count-1].StripeColor := Random($FFFFFF);
+end;
+
+procedure TForm1.ExtTabCtrl1TabCreating(Sender: TObject; var ACaption: String;
+  var Data: TObject; var Allow: Boolean);
+var
+  idx: Integer;
+begin
+  idx := Notebook1.Pages.Add(ACaption);
+  Notebook1.Page[idx].Color := Random($FFFFFF);
   Allow := True;
 end;
 
-procedure TForm1.ExtTabCtrl1TabDeleting(Sender: TObject; Index: Integer; var Allow: Boolean);
+procedure TForm1.ExtTabCtrl1TabDeleting(Sender: TObject; Index: Integer;
+  var Allow: Boolean);
 begin
   if (Index >= 0) and (Index < Notebook1.PageCount) then
   begin
@@ -75,10 +99,12 @@ begin
   end;
 end;
 
-procedure TForm1.ExtTabCtrl1TabReordered(Sender: TObject; OldIndex, NewIndex: Integer);
+procedure TForm1.ExtTabCtrl1TabReordered(Sender: TObject; OldIndex,
+  NewIndex: Integer);
 begin
   if (OldIndex >= 0) and (OldIndex < Notebook1.PageCount) and
-     (NewIndex >= 0) and (NewIndex < Notebook1.PageCount) then
+     (NewIndex >= 0) and (NewIndex < Notebook1.PageCount)
+  then
     Notebook1.Pages.Move(OldIndex, NewIndex);
 end;
 
@@ -87,10 +113,43 @@ begin
   ExtTabCtrl1.TabStyle := TTabStyle(CmbTabStyle.ItemIndex);
 end;
 
+procedure TForm1.cbShowAddButtonChange(Sender: TObject);
+begin
+  if cbShowAddButton.Checked then
+    ExtTabCtrl1.TabOptions := ExtTabCtrl1.TabOptions + [toShowAddButton]
+  else
+    ExtTabCtrl1.TabOptions := ExtTabCtrl1.TabOptions - [toShowAddButton];
+end;
+
 procedure TForm1.Button1Click(Sender: TObject);
 begin
   if OpenPictureDialog1.Execute then
     LoadImage(OpenPictureDialog1.FileName);
+end;
+
+procedure TForm1.cbShowCloseButtonsChange(Sender: TObject);
+begin
+  if cbShowCloseButtons.Checked then
+    ExtTabCtrl1.TabOptions := ExtTabCtrl1.TabOptions + [toShowCloseButton]
+  else
+    ExtTabCtrl1.TabOptions := ExtTabCtrl1.TabOptions - [toShowCloseButton];
+end;
+
+procedure TForm1.cbShowTabStripesChange(Sender: TObject);
+const
+  COLORS: array[0..5] of TColor = (clRed, clLime, clYellow, clBlue, clFuchsia, clAqua);
+var
+  i: Integer;
+  tab: TExtTab;
+begin
+  for i := 0 to ExtTabCtrl1.Tabs.Count-1 do
+  begin
+    tab := ExtTabCtrl1.Tabs[i];
+    if cbShowTabStripes.Checked then
+      tab.StripeColor := COLORS[i mod Length(COLORS)]
+    else
+      tab.StripeColor := clNone;
+  end;
 end;
 
 procedure TForm1.LoadImage(const AFileName: String);
