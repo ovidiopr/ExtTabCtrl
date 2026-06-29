29c84f3bec453f040af03d2063967ab131951b6c
Ovidio Peña Rodríguez <ovidiopr@users.noreply.github.com>
2026-06-29 17:13:19 +0200

    Completed example

--- examples/demo_ExtPageCtrl/umain.pas
+++ examples/demo_ExtPageCtrl/umain.pas
@@ -6,18 +6,31 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls,
-  ExtPageCtrl;
+  ExtTabCtrl, ExtPageCtrl;
 
 type
 
   { TForm1 }
 
   TForm1 = class(TForm)
-    Button1: TButton;
+    cbRotateNewTabBtn: TCheckBox;
+    cbRotateTabIcons: TCheckBox;
+    cbShowNewTabBtn: TCheckBox;
+    cbUseExternalImages: TCheckBox;
     ExtPage1: TExtPage;
     ExtPage2: TExtPage;
+    ExtPage3: TExtPage;
     ExtPageCtrl1: TExtPageCtrl;
-    ToggleBox1: TToggleBox;
+    ImageList1: TImageList;
+    RadioGroup1: TRadioGroup;
+    RadioGroup2: TRadioGroup;
+    procedure cbRotateNewTabBtnChange(Sender: TObject);
+    procedure cbRotateTabIconsChange(Sender: TObject);
+    procedure cbShowNewTabBtnChange(Sender: TObject);
+    procedure cbUseExternalImagesChange(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure RadioGroup1Click(Sender: TObject);
+    procedure RadioGroup2Click(Sender: TObject);
   private
 
   public
@@ -30,6 +43,66 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.RadioGroup1Click(Sender: TObject);
+begin
+  ExtPageCtrl1.BeginUpdate;
+  try
+    ExtPageCtrl1.TabPosition := TExtTabPosition(RadioGroup1.ItemIndex);
+    ExtPageCtrl1.Align := TAlign(RadioGroup1.ItemIndex + 1);
+  finally
+    ExtPageCtrl1.EndUpdate;
+  end;
+end;
+
+procedure TForm1.RadioGroup2Click(Sender: TObject);
+begin
+  ExtPageCtrl1.TabStyle := TExtTabStyle(RadioGroup2.ItemIndex);
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  RadioGroup1.ItemIndex := Ord(ExtPageCtrl1.TabPosition);
+  RadioGroup2.ItemIndex := Ord(ExtPageCtrl1.TabStyle);
+  cbShowNewTabBtn.Checked := etoShowAddButton in ExtPageCtrl1.TabOptions;
+  cbRotateNewTabBtn.Checked := etoRotateAddImage in ExtPageCtrl1.TabOptions;
+  cbUseExternalImages.Checked := ExtPageCtrl1.Images <> nil;
+  cbRotateTabIcons.Checked := etoRotateTabImages in ExtPageCtrl1.TabOptions;
+end;
+
+procedure TForm1.cbShowNewTabBtnChange(Sender: TObject);
+begin
+  if cbShowNewTabBtn.Checked then
+    ExtPageCtrl1.TabOptions := ExtPageCtrl1.TabOptions + [etoShowAddButton]
+  else
+    ExtPageCtrl1.TabOptions := ExtPageCtrl1.TabOptions - [etoShowAddButton];
+end;
+
+procedure TForm1.cbUseExternalImagesChange(Sender: TObject);
+begin
+  if cbUseExternalImages.Checked then
+    ExtPageCtrl1.Images := ImageList1
+  else
+    ExtPageCtrl1.Images := nil;
+end;
+
+procedure TForm1.cbRotateNewTabBtnChange(Sender: TObject);
+begin
+  if cbRotateNewTabBtn.Checked then
+    ExtPageCtrl1.TabOptions := ExtPageCtrl1.TabOptions + [etoRotateAddImage]
+  else
+    ExtPageCtrl1.TabOptions := ExtPageCtrl1.TabOptions - [etoRotateAddImage];
+end;
+
+procedure TForm1.cbRotateTabIconsChange(Sender: TObject);
+begin
+  if cbRotateTabIcons.Checked then
+    ExtPageCtrl1.TabOptions := ExtPageCtrl1.TabOptions + [etoRotateTabImages]
+  else
+    ExtPageCtrl1.TabOptions := ExtPageCtrl1.TabOptions - [etoRotateTabImages];
+end;
 
 end.
 
